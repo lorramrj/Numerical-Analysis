9064059febc885f2502274c9a3e1f58e6a9cd888
Lorram <lorramrj@gmail.com>
2021-09-05 19:06:59 -0300

    ajustes...

--- Lab3/sistlinear.h
+++ Lab3/sistlinear.h
@@ -1,7 +1,4 @@
 
 void fatoracao (int n, double** A, int* p);
-void fatoracao2 (int n, double** A, int* p);
-
 void substituicao (int n, double** A, int* p, double* b, double* x);
-
 void gauss (int n, double** A, double* b, double* x);
